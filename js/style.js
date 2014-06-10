db2bbc6c34a084c3b6f50f22641a85ff177ae09c
koyota <koyota@gmail.com>
2014-06-10 16:17:57 +0800

    adding cats and tables

---
+++ js/style.js
@@ -0,0 +1,3 @@
+$('#cat-button')
+  .mouseenter(function(){$('#asks').attr('src', 'http://dummyimage.com/100x100/eb00eb/fff');})
+  .mouseleave(function(){$('#asks').attr('src', 'http://dummyimage.com/100x100/000/fff');});
